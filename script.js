3f63a9ecf7e1a36d4e83d4e01440b96d0b16156d
Celina Louissaint <celinalouissaint@gmail.com>
2020-12-12 18:05:15 -0800

    mobile responsive css added

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-var textWrapper = document.querySelector('.effect1');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^.\s]|\w)/g, "<span class='letter'>$&</span>");
+const arrowButton = document.querySelector('.round')
+const contact = document.querySelector('#contact')
 
-anime.timeline()
-  .add({
-    targets: '.effect1 .letter',
-    scale: [5,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 1350,
-    delay: function(el, i) {
-      return 70*i;
-  }
-}, 1500);
+arrowButton.addEventListener('click', function(){
+  contact.scrollIntoView();
+})
